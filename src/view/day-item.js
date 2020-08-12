ff868bc1ed99e871785f80cbdde25b70ff66e7b4
Serenya Dyadev <3684182@gmail.com>
2020-08-12 14:08:39 +0300

    рефакторинг

--- src/view/day-item.js
+++ src/view/day-item.js
@@ -4,21 +4,26 @@ import {getDayMonthStamp, getYearMonthDayStamp} from "../date-utils.js";
 const getEventsTemplate = (events, count, day) => {
 
   return new Array(count).fill().map((element, index) => {
-    return day === getDayMonthStamp(events[index]) ? createEventItem(events[index]) : ``;
+    return day === getDayMonthStamp(events[index].time[0]) ? createEventItem(events[index]) : ``;
   }).join(` `);
 
 };
 
-export const createDayItem = (events, count) => {
-
+const getSortDatesEndDaysForTemplate = (events) => {
   const daysForTemplate = {};
-  for (let i = 1; i < events.length; i++) {
-    let key = getDayMonthStamp(events[i]);
-    daysForTemplate[key] = getYearMonthDayStamp(events[i]);
+  for (let i = 0; i < events.length; i++) {
+    let key = getDayMonthStamp(events[i].time[0]);
+    daysForTemplate[key] = getYearMonthDayStamp(events[i].time[0]);
   }
 
   const days = Object.keys(daysForTemplate).sort();
   const dates = Object.values(daysForTemplate).sort();
+
+  return {days, dates};
+};
+
+export const createDayItem = (events, count) => {
+  const {days, dates} = getSortDatesEndDaysForTemplate(events);
 
   return new Array(days.length).fill().map((element, index) =>
     `<li class="trip-days__item  day">

f8559c3ed35715339d42457ec560a708aa00ce16
Serenya Dyadev <3684182@gmail.com>
2020-09-14 13:07:43 +0300

    m9-t1

---
+++ src/api/provider.js
@@ -0,0 +1,106 @@
+import {nanoid} from "nanoid";
+import EventsModel from "../model/points.js";
+
+const getSyncedEvents = (items) => {
+  return items.filter(({success}) => success)
+    .map(({payload}) => payload.event);
+};
+
+const createStoreStructure = (items) => {
+  return items.reduce((acc, current) => {
+    return Object.assign({}, acc, {
+      [current.id]: current,
+    });
+  }, {});
+};
+
+export default class Provider {
+  constructor(api, store) {
+    this._api = api;
+    this._store = store;
+  }
+
+  getEvents() {
+    if (Provider.isOnline()) {
+      return this._api.getEvents()
+        .then((events) => {
+          const items = createStoreStructure(events.map(EventsModel.adaptToServer));
+          this._store.setItems(items);
+          return events;
+        });
+    }
+
+    const storeEvents = Object.values(this._store.getItems());
+
+    return Promise.resolve(storeEvents.map(EventsModel.adaptToClient));
+  }
+
+  updateEvent(event) {
+    if (Provider.isOnline()) {
+      return this._api.updateEvent(event)
+        .then((updatedEvent) => {
+          this._store.setItem(updatedEvent.id, EventsModel.adaptToServer(updatedEvent));
+          return updatedEvent;
+        });
+    }
+
+    this._store.setItem(event.id, EventsModel.adaptToServer(Object.assign({}, event)));
+
+    return Promise.resolve(event);
+  }
+
+  addEvent(event) {
+    if (Provider.isOnline()) {
+      return this._api.addEvent(event)
+        .then((newEvent) => {
+          this._store.setItem(newEvent.id, EventsModel.adaptToServer(newEvent));
+          return newEvent;
+        });
+    }
+
+    // На случай локального создания данных мы должны сами создать `id`.
+    // Иначе наша модель будет не полной, и это может привнести баги
+    const localNewEventId = nanoid();
+    const localNewEvent = Object.assign({}, event, {id: localNewEventId});
+
+    this._store.setItem(localNewEvent.id, EventsModel.adaptToServer(localNewEvent));
+
+    return Promise.resolve(localNewEvent);
+  }
+
+  deleteEvent(event) {
+    if (Provider.isOnline()) {
+      return this._api.deleteEvent(event)
+        .then(() => this._store.removeItem(event.id));
+    }
+
+    this._store.removeItem(event.id);
+
+    return Promise.resolve();
+  }
+
+  sync() {
+    if (Provider.isOnline()) {
+      const storeEvents = Object.values(this._store.getItems());
+
+      return this._api.sync(storeEvents)
+        .then((response) => {
+          // Забираем из ответа синхронизированные задачи
+          const createdEvents = getSyncedEvents(response.created);
+          const updatedEvents = getSyncedEvents(response.updated);
+
+          // Добавляем синхронизированные задачи в хранилище.
+          // Хранилище должно быть актуальным в любой момент.
+          const items = createStoreStructure([...createdEvents, ...updatedEvents]);
+
+          this._store.setItems(items);
+        });
+    }
+
+    return Promise.reject(new Error(`Sync data failed`));
+  }
+
+  static isOnline() {
+    return window.navigator.onLine;
+  }
+}
--- src/const.js
+++ src/const.js
@@ -19,39 +19,6 @@ export const EVENT_TYPES = [
   PLACES
 ];
 
-export const DESTINATIONS = [
-  `Amsterdam`,
-  `Geneva`,
-  `Chamonix`,
-  `Saint Petersburg`
-];
-
-export const DESCRIPTIONS = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-  `Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus.`,
-  `In rutrum ac purus sit amet tempus.`
-];
-
-
-export const OFFER_LIST = {
-  "taxi": [`Order Uber`, `Taxi Minivan`, `Business Class`],
-  "bus": [`Comfort Chair`, `Bisiness Chair`],
-  "ship": [`Lux`],
-  "transport": [`Comfort ticket`, `Bisiness ticket`],
-  "drive": [`Motorbike`, `Sedan`, `MiniBus`],
-  "flight": [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`, `Travel by train`],
-  "check-in": [],
-  "sightseeing": [],
-  "restaurant": [`Two persons`, `Dinner`],
-  "train": [`VIP`, `Tea`, `Comfort cupe`]
-};
-
 export const SortType = {
   EVENT: `event`,
   TIME: `time`,
